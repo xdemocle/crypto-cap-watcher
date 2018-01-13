8f79426be1fc328d9fc6bd33a80b2d976b42c3f4
xdemocle <xdemocle@gmail.com>
2018-01-13 17:59:00 +0200

    adding CNAME to master and create a copy task in webpack for it

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -138,6 +138,11 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../CNAME'),
+        to: config.build.assetsRoot,
+        ignore: ['.*']
       }
     ])
   ]
