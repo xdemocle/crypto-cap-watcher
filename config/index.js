9f09c4c1b325dc700dcafe8eec3647afb8b6a555
xdemocle <xdemocle@gmail.com>
2018-06-05 05:13:59 +0200

    minor changes in webpack project; update vue chromecast plugin; bump versopn for new CAF sdk chromecast

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ module.exports = {
     // Various Dev Server settings
     host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    https: true,
+    https: false,
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
